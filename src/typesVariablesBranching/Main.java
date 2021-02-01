6e46e00309e7d3c8b941dfd09cacd7bede6c92bf
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-01 11:10:47 -0500

    Comment changes

--- src/typesVariablesBranching/Main.java
+++ src/typesVariablesBranching/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         String sentence; //Uninitialized
         char letter = 'A';
-        int 1number = 50; //Variable cannot start with number
+        int 1number = 50; //Variable cannot start with number, syntax error
 
         System.out.println("This is my sentence: " + sentence); //Uninitialized error
         System.out.println("This is my letter: " + letter);
-        System.out.println("This is my number: " + 1number); //Syntax error
+        System.out.println("This is my number: " + 1number); //Will not reach this statement due to error
     }
 }
