00b3df0746d0a8c68e499ee95ce312f19f00e1b8
Elijah Munroe <elimunroe15@hotmail.com>
2021-02-01 11:08:48 -0500

    Create syntax and uninitialized variables

--- src/typesVariablesBranching/Main.java
+++ src/typesVariablesBranching/Main.java
@@ -2,6 +2,12 @@ package typesVariablesBranching;
 
 public class Main {
     public static void main(String[] args) {
+        String sentence; //Uninitialized
+        char letter = 'A';
+        int 1number = 50; //Variable cannot start with number
 
+        System.out.println("This is my sentence: " + sentence); //Uninitialized error
+        System.out.println("This is my letter: " + letter);
+        System.out.println("This is my number: " + 1number); //Syntax error
     }
 }
